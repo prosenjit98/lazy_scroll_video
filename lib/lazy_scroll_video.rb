c0797e9137e8c8ea9aec3b51fdfb49ff3ab20751
Prosenjit Chongder <prosenjit.chongder@geogo.in>
2025-04-09 18:33:21 +0530

    first commit

---
+++ lib/lazy_scroll_video.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "lazy_scroll_video/version"
+
+module LazyScrollVideo
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "lazy_scroll_video"
+
+require "minitest/autorun"
